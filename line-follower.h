21a86ffb7d20a812c38774de9db7cc416e900ccc
Dunghedspi <dung.nv.soict@gmail.com>
2021-06-08 09:17:35 +0700

    fix_error

---
+++ line-follower.h
@@ -0,0 +1,13 @@
+#define enA 3
+#define enB 5
+
+#define inA1 6 //Định nghĩa chân in1 của động cơ A // A is left tire
+#define inA2 7 //Định nghĩa chân in2 của động cơ A
+#define inB1 8 //Định nghĩa chân in1 của động cơ B // B is right tire
+#define inB2 9 //Định nghĩa chân in2 của động cơ B
+
+#define linesens1 10 //Định nghĩa chân cảm biến line 1
+#define linesens2 11 //Định nghĩa chân cảm biến line 2
+#define linesens3 12 //Định nghĩa chân cảm biến line 3
+
+#define MAX_SPEED 120
--- line-follower.ino
+++ line-follower.ino
@@ -1,18 +1,4 @@
-#define enA 3
-#define enB 5
-
-#define inA1 6 //Định nghĩa chân in1 của động cơ A // A is left tire
-#define inA2 7 //Định nghĩa chân in2 của động cơ A
-#define inB1 8 //Định nghĩa chân in1 của động cơ B // B is right tire
-#define inB2 9 //Định nghĩa chân in2 của động cơ B
-
-#define linesens1 10 //Định nghĩa chân cảm biến line 1
-#define linesens2 11 //Định nghĩa chân cảm biến line 2
-#define linesens3 12 //Định nghĩa chân cảm biến line 3
-
-#define MAX_SPEED 120
-
-
+#include "line-follower.h"
 int statusCar = 0; // default 0 
 
 void setup() {
@@ -121,7 +107,6 @@ void motorControlNoSpeed (byte in1, byte in2, byte direct)
       analogWrite(enB, MAX_SPEED);
       
       break;
-      //default:
   }
 }
 
@@ -146,8 +131,6 @@ void motorControlWithSpeed (byte in1, byte in2, byte direct, int spdA, int spdB)
       if(spdA != 0) analogWrite(enA, spdA);
       if(spdB != 0) analogWrite(enB, spdB);
       break;
-      
-      //default:
   }
 }
 
